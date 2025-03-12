483c750aa8c47f4e53261b8444b7f02921011285
LIGECT <viminiko@gmail.com>
2025-03-12 22:38:17 +0200

    Write logic for a one-round game

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-function getComputerChoice() {
-  let choice1 = "Rock";
-  let choice2 = "Paper";
-  let choice3 = "Scissors";
+let humanScore = 0;
+let computerScore = 0;
+let choice1 = "Камень";
+let choice2 = "Ножницы";
+let choice3 = "Бумага";
 
+function getComputerChoice() {
   let random = Math.floor(Math.random() * 3);
 
   if (random === 0) {
@@ -14,4 +16,40 @@ function getComputerChoice() {
   }
 }
 
-console.log(getComputerChoice());
+function getHumanChoice() {
+  let choice = prompt("Ведите ваш выбор: Камень, Ножницы или Бумага").trim();
+  return choice;
+}
+
+function playRound(humanChoiсe, computerChoiсe) {
+  //code
+  humanChoiсe =
+    humanChoiсe.at(0).toUpperCase() + humanChoiсe.slice(1).toLowerCase();
+
+  computerChoiсe =
+    computerChoiсe.at(0).toUpperCase() + computerChoiсe.slice(1).toLowerCase();
+
+  if (humanChoiсe === computerChoiсe) {
+    console.log("Ничья");
+    console.log("Счет: " + humanScore + " - " + computerScore);
+  } else if (humanChoiсe === "Камень" && computerChoiсe === "Ножницы") {
+    humanScore++;
+    console.log("Ты выиграл!");
+    console.log("Счет: " + humanScore + " - " + computerScore);
+  } else if (humanChoiсe === "Ножницы" && computerChoiсe === "Бумага") {
+    humanScore++;
+    console.log("Ты выиграл!");
+    console.log("Счет: " + humanScore + " - " + computerScore);
+  } else if (humanChoiсe === "Бумага" && computerChoiсe === "Камень") {
+    humanScore++;
+    console.log("Ты выиграл!");
+    console.log("Счет: " + humanScore + " - " + computerScore);
+  } else {
+    computerScore++;
+    console.log("Компьютер выиграл");
+    console.log("Счет: " + humanScore + " - " + computerScore);
+  }
+}
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+playRound(humanSelection, computerSelection);
